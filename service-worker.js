129c4e6e894880f63ac28c51de18c78d2caec1e3
Fahad Rana <31787094+fahadmajeedrana@users.noreply.github.com>
2026-09-18 19:23:20 +1200

    Update cache version from v1 to v2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'strength-v1';
+const CACHE = 'strength-v2';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
 });
